42895a21e540bf11b5f2097af00393037a215ec2
vishnuthatiparthi <vishnureddys12345@gmail.com>
2025-11-24 22:20:47 +0530

    terraform

--- stack-1(cheetah-app)/s3.tf
+++ stack-1(cheetah-app)/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "be_app_bucket" {
-  bucket = "cheetah-${var.envname}-be-app-bucket-demo"
+  bucket = "cheetah-dev-be-app-bucket-demo"
 }
 
 resource "aws_s3_bucket" "fe_app_bucket" {
-  bucket = "cheetah-${var.envname}-fe-app-bucket-demo"
+  bucket = "cheetah-dev-fe-app-bucket-demo"
 }
